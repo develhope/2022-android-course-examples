057794e05b153931a903ad3d67b229fb03ba3369
Sasa Sekulic <ja@sasasekulic.info>
2022-04-13 19:41:46 +0200

    added 4.4 networking with retrofit example

---
+++ 4 - networking/4.4 networking with retrofit/app/src/main/java/com/example/myapplication/RepoAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication.github.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+
+class RepoViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    val repoName: TextView
+
+    init {
+        repoName = view.findViewById(R.id.repo_name)
+    }
+}
+
+class RepoAdapter(val repoResults: List<Repo>): RecyclerView.Adapter<RepoViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
+        val repoView = LayoutInflater.from(parent.context).inflate(R.layout.repolistitem, parent, false)
+        return RepoViewHolder(repoView)
+    }
+
+    override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
+        holder.repoName.text = repoResults[position].name
+    }
+
+    override fun getItemCount(): Int {
+        return repoResults.size
+    }
+}
